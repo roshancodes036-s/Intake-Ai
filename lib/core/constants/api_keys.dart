bcc7f6e68c4caae2422253221e15720674c0324d
roshancodes036-su <roshancodes036@gmail.com>
2026-03-12 10:27:41 +0000

    Initial commit

--- lib/core/constants/api_keys.dart
+++ lib/core/constants/api_keys.dart
@@ -1,6 +1,6 @@
 class ApiKeys {
   // Google AI Studio से अपनी Gemini API Key जेनरेट करके यहाँ डालें
-  static const String geminiApiKey = 'AIzaSyDKCI0ngb7hEQaIfiGWayFAO7rupvL5zvY';
+  static const String geminiApiKey = 'your api key here ';
 
   // अगर भविष्य में आप कोई अलग Nutrition API या Firebase इस्तेमाल करते हैं,
   // तो उसकी keys भी यहाँ रख सकते हैं:
